86d9d2bf7b2d36f93312bf37f269e747441c1cb0
Mouse Reeve <mousereeve@riseup.net>
2017-03-14 13:13:50 -0700

    A variety of responses

--- catms.py
+++ catms.py
@@ -1,6 +1,7 @@
 ''' get sms from a cat '''
 from flask import Flask, request
-import random
+import re
+from random import choice, randint
 from twilio.rest import TwilioRestClient
 import settings
 
@@ -27,12 +28,38 @@ def web_responder():
     return meow()
 
 
-def meow():
+def meow(message=u''):
     ''' the cat's response '''
-    return 'm' * random.randint(1, 4) + \
-           'r' * random.randint(2, 8) + \
-           'o' * random.randint(1, 9) + \
-           'w'
+
+    responses = {
+        r'.*\bselfie\b.*': u'\U0001F431',
+        r'.*are you a [cat|kitty|kitling|kitten].*': u'\U0001F431' * 3,
+        u'.*I (love|\u2764) you.*': u'{meow} \u2764'
+    }
+
+    reply = '{meow}'
+
+    if len(message) > 3:
+        for (regex, response) in responses.items():
+            if re.match(regex, message, re.IGNORECASE):
+                reply = response
+                break
+
+    meows = [
+        'm' * randint(1, 2) + 'r' * randint(2, 8) + 'o' * randint(1, 10) + 'w',
+        'merp' + ' erp' * randint(0, 3),
+        'e' * randint(3, 5) + 'a' * randint(1, 5) + 'wwr' + 'a' * randint(2, 5),
+        'm' + 'e' * randint(2, 7) + 'p',
+        'mrp mir mrr' + 'e' * randint(3, 6) + 'p',
+        'm' + 'r' * randint(5, 10),
+        'a' * randint(1, 3) + 'ww' + 'r' * randint(3, 5) + 'a' * randint(2, 10),
+        'm' + 'rm' * randint(3, 6),
+        'snxr' + 'x' * randint(1, 4),
+    ]
+
+    reply = re.sub(r'{meow}', choice(meows), reply)
+
+    return reply
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3333)
